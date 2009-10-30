edd463ef2a549af49d4692db5858dbf73a1eb817
attila.lendvai <attila.lendvai@gmail.com>
2009-10-30 19:05:28 +0000

    fix: lambda list walking did not update the env (thinko in recent refactorings)

--- source/function.lisp
+++ source/function.lisp
@@ -180,7 +180,7 @@
   (:method :before (ast-node args body env &key &allow-other-keys)
     (check-type env walk-environment))
   (:method (ast-node args body env &key docstring-allowed declarations-allowed (whole *current-form*))
-    (setf (arguments-of ast-node) (walk-ordinary-lambda-list args ast-node env))
+    (setf (values (arguments-of ast-node) env) (walk-ordinary-lambda-list args ast-node env))
     (walk-implict-progn ast-node body env
                         :docstring-allowed docstring-allowed :declarations-allowed declarations-allowed
                         :whole whole)
@@ -210,7 +210,7 @@
     (dolist (parsed result)
       (unless (typep parsed 'allow-other-keys-function-argument-form)
         (augment-walk-environment! env :variable (name-of parsed) parsed)))
-    result))
+    (values result env)))
 
 (def (class* ea) function-argument-form (named-walked-form)
   ())
