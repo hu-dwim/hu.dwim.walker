e3d20fe0467c6e02ba0b606970aec1aa2d94921e
levente.meszaros <levente.meszaros@gmail.com>
2010-03-23 21:27:41 +0000

    Follow wui

--- documentation/walker.lisp
+++ documentation/walker.lisp
@@ -9,9 +9,9 @@
 (def project :hu.dwim.walker :path (system-pathname :hu.dwim.walker))
 
 (def method make-project-tab-pages ((component project/detail/inspector) (project (eql (find-project :hu.dwim.walker))))
-  (append (list (tab-page/widget (:selector (icon switch-to-tab-page :label "User guide"))
+  (append (list (tab-page/widget (:selector (icon/widget switch-to-tab-page :label "User guide"))
                   (make-value-inspector (find-book 'user-guide)))
-                (tab-page/widget (:selector (icon switch-to-tab-page :label "Dictionary"))
+                (tab-page/widget (:selector (icon/widget switch-to-tab-page :label "Dictionary"))
                   (make-value-inspector (mapcar 'find-dictionary '(walker)))))
           (call-next-method)))
 
